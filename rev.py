182fb7064b9e145ffc2b81bfef993cecbf3140c5
Geoff Lankow <geoff@darktrojan.net>
2023-06-08 16:03:57 +1200

    Update revision finder

--- rev.py
+++ rev.py
@@ -1,29 +1,50 @@
 import argparse
 import subprocess
-import sys
-import urllib.request
+import urllib3
 import yaml
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--update', action='store_true')
-parser.add_argument('rev', nargs='?')
+parser.add_argument("-u", "--update", action="store_true")
+parser.add_argument("rev", nargs="?")
 args = parser.parse_args()
 
 if args.rev is not None:
-  hg_data = subprocess.run(['hg', 'log', '--rev', args.rev, '--template', '{node}'], capture_output=True)
+    hg_data = subprocess.run(
+        ["hg", "log", "--rev", args.rev, "--template", "{node} {fxheads}"], capture_output=True
+    )
 else:
-  hg_data = subprocess.run(['hg', 'log', '--rev', 'last(public() and ancestors(.))', '--template', '{node}'], capture_output=True)
+    hg_data = subprocess.run(
+        [
+            "hg",
+            "log",
+            "--rev",
+            "last(public() and ancestors(.))",
+            "--template",
+            "{node} {fxheads}",
+        ],
+        capture_output=True,
+    )
 
 hg_data.check_returncode()
-rev = hg_data.stdout.decode()
-print('c-c revision is', rev)
+hg_output = hg_data.stdout.decode().split()
+rev = None
+head = None
+if len(hg_output) == 2:
+    [rev, head] = hg_output
+else:
+    [rev] = hg_output
+tree = head if head in ["ash", "comm-beta", "comm-esr102"] else "comm-central"
+print(tree, "revision is", rev)
 
-url = 'https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/comm.v2.comm-central.revision.{rev}.taskgraph.decision/artifacts/public/parameters.yml'
+url = "https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/comm.v2.{tree}.revision.{rev}.taskgraph.decision/artifacts/public/parameters.yml"
 
-res = urllib.request.urlopen(url.format(rev=rev))
-yaml_data = res.read()
+res = urllib3.PoolManager().request("GET", url.format(tree=tree, rev=rev))
+yaml_data = res.data
 data = yaml.load(yaml_data, Loader=yaml.CLoader)
-print('m-c revision is', data.get('head_rev'))
+print("mozilla-central revision is", data.get("head_rev"))
 
 if args.update:
-  subprocess.run(['hg', '-R', '..', 'pull', '-u', '-r', data.get('head_rev')])
+    hg_data = subprocess.run(["hg", "-R", "..", "pull", "-r", data.get("head_rev")])
+    hg_data.check_returncode()
+    hg_data = subprocess.run(["hg", "-R", "..", "update", data.get("head_rev")])
+    hg_data.check_returncode()
